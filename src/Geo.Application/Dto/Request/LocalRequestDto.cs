e50708ea20ef500d886bc4fba6f3c4cb8d6c8abb
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-01-03 12:39:30 -0300

    adicionado DataAnnotations

--- src/Geo.Application/Dto/Request/LocalRequestDto.cs
+++ src/Geo.Application/Dto/Request/LocalRequestDto.cs
@@ -1,10 +1,18 @@
-﻿using System.Drawing;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
 
 namespace Geo.Application.Dto.Request;
 
 public class LocalRequestDto
 {
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    [StringLength(75, ErrorMessage = "O nome deve ter no máximo 75 caracteres.")]
     public string Nome { get; set; }
+
+    [Required(ErrorMessage = "A categoria é obrigatória.")]
+    [RegularExpression("^(Farmacia|Restaurante|Hospital|Outro)$", ErrorMessage = "Categoria inválida. Categorias permitidas: Farmacia, Restaurante, Hospital, Outro.")]
     public string Categoria { get; set; }
+
+    [Required(ErrorMessage = "As coordenadas são obrigatórias.")]
     public Point Coordenadas { get; set; }
 }
