070bb8817a3a8688b25da337d89e15311c152164
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-01-03 11:37:13 -0300

    criação da interface

---
+++ src/Geo.Domain/Interface/ILocalRepository.cs
@@ -0,0 +1,12 @@
+﻿using Geo.Domain.Models;
+
+namespace Geo.Domain.Interface;
+
+internal interface ILocalRepository
+{
+    Task<Local> BuscarLocalPorId(Guid id);
+    Task<IEnumerable<Local>> BuscarTodos();
+    Task<Local> Criar(Local local);
+    Task<Local> Atualizar(Local local);
+    Task<bool> Deletar(Guid id);
+}
