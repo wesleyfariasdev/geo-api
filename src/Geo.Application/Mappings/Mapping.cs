67eacb75d9071e8dd6ff796b596aace6fda53ec8
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-01-03 12:09:34 -0300

    add mappings dto e models

---
+++ src/Geo.Application/Mappings/Mapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Geo.Application.Dto.Request;
+using Geo.Application.Dto.Response;
+using Geo.Domain.Models;
+
+namespace Geo.Application.Mappings;
+
+internal class Mapping : Profile
+{
+    public Mapping()
+    {
+        CreateMap<Local, LocalRequestDto>().ReverseMap();
+        CreateMap<Local, LocalResponseDto>().ReverseMap();
+    }
+}
