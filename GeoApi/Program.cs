2b33679d7de96b603b4afbcc1b5a4b68dcedce2b
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-01-03 12:40:22 -0300

    adicionado injeção de dependencia

--- GeoApi/Program.cs
+++ GeoApi/Program.cs
@@ -1,6 +1,9 @@
+using Geo.Infra.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddIoCServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
