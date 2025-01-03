5741e68cd46e37d477c5b70be8371a689d2d289f
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-01-03 12:09:17 -0300

    add dtos para request e response

---
+++ src/Geo.Application/Dto/Request/LocalRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace Geo.Application.Dto.Request;
+
+internal class LocalRequestDto
+{
+    public string Nome { get; set; }
+    public string Categoria { get; set; }
+    public Point Coordenadas { get; set; }
+}
---
+++ src/Geo.Application/Dto/Response/LocalResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace Geo.Application.Dto.Response;
+
+internal class LocalResponseDto
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public string Categoria { get; set; }
+    public Point Coordenadas { get; set; }
+}
