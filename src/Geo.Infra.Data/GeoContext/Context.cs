73827a7186bc6fb8e060926c4d24a386cb15d1f8
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-01-03 11:55:19 -0300

    adicionado context

---
+++ src/Geo.Infra.Data/GeoContext/Context.cs
@@ -0,0 +1,14 @@
+﻿using Geo.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Geo.Infra.Data.GeoContext;
+
+public class Context(DbContextOptions<Context> options) : DbContext(options)
+{
+    public DbSet<Local> Locais { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(Context).Assembly);
+    }
+}
