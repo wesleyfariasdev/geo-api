e6fff6e6ed693f7314524453f7a18ce996f5447e
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-01-03 12:19:38 -0300

    add IoC

---
+++ src/Geo.Infra.IoC/IoC.cs
@@ -0,0 +1,24 @@
+﻿using Geo.Application.Services;
+using Geo.Application.Services.IService;
+using Geo.Domain.Interface;
+using Geo.Infra.Data.GeoContext;
+using Geo.Infra.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Geo.Infra.IoC;
+
+public static class IoC
+{
+    public static IServiceCollection AddIoCServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<Context>(options => options
+         .UseNpgsql(config.GetConnectionString("Dev"), x => x
+             .MigrationsAssembly(typeof(Context).Assembly.FullName)));
+
+        services.AddScoped<ILocalRepository, LocalRepository>();
+        services.AddScoped<ILocalService, LocalServices>();
+        return services;
+    }
+}
