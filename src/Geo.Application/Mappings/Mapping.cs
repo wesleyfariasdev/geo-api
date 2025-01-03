9fab1fac9c4bf12008d0e316c22f96566333927e
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-01-03 17:10:48 -0300

    mapeia simpleificação de Point pra CoordenadasDto

--- src/Geo.Application/Mappings/Mapping.cs
+++ src/Geo.Application/Mappings/Mapping.cs
@@ -1,15 +1,32 @@
 ﻿using AutoMapper;
+using Geo.Application.Dto;
 using Geo.Application.Dto.Request;
 using Geo.Application.Dto.Response;
 using Geo.Domain.Models;
+using NetTopologySuite.Geometries;
 
 namespace Geo.Application.Mappings;
+
 
 public class Mapping : Profile
 {
     public Mapping()
     {
-        CreateMap<Local, LocalRequestDto>().ReverseMap();
-        CreateMap<Local, LocalResponseDto>().ReverseMap();
+        // Mapeamento do Point para CoordenadasDto
+        CreateMap<Point, CoordenadasDto>()
+            .ForMember(dest => dest.Latitude, opt => opt.MapFrom(src => src.Y))
+            .ForMember(dest => dest.Longitude, opt => opt.MapFrom(src => src.X))
+            .ReverseMap()
+            .ConstructUsing(src => new Point(src.Longitude, src.Latitude) { SRID = 4326 });
+
+        // Mapeamento de Local para LocalRequestDto
+        CreateMap<Local, LocalRequestDto>()
+            .ForMember(dest => dest.Coordenadas, opt => opt.MapFrom(src => src.Coordenadas))
+            .ReverseMap()
+            .ForMember(dest => dest.Coordenadas, opt => opt.MapFrom(src => src.Coordenadas));
+
+        // Mapeamento de Local para LocalResponseDto
+        CreateMap<Local, LocalResponseDto>()
+            .ForMember(dest => dest.Coordenadas, opt => opt.MapFrom(src => src.Coordenadas));
     }
 }
