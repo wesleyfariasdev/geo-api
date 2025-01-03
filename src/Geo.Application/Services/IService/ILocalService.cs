027cef01c9b42ce5cc43a2a4089c1e00326a3284
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-01-03 12:09:44 -0300

    add services

---
+++ src/Geo.Application/Services/IService/ILocalService.cs
@@ -0,0 +1,13 @@
+﻿using Geo.Application.Dto.Request;
+using Geo.Application.Dto.Response;
+
+namespace Geo.Application.Services.IService;
+
+internal interface ILocalService
+{
+    Task<LocalResponseDto> BuscarLocalPorId(Guid id);
+    Task<IEnumerable<LocalResponseDto>> BuscarTodos();
+    Task<LocalResponseDto> Criar(LocalRequestDto local);
+    Task<LocalResponseDto> Atualizar(LocalRequestDto local);
+    Task<bool> Deletar(Guid id);
+}
---
+++ src/Geo.Application/Services/LocalServices.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Geo.Application.Dto.Request;
+using Geo.Application.Dto.Response;
+using Geo.Application.Services.IService;
+using Geo.Domain.Interface;
+using Geo.Domain.Models;
+
+namespace Geo.Application.Services;
+
+internal class LocalServices(ILocalRepository _localRepository, IMapper _mapper) : ILocalService
+{
+    public async Task<LocalResponseDto> Criar(LocalRequestDto localDto)
+    {
+        if (localDto == null) throw new ArgumentNullException(nameof(localDto));
+
+        var localEntity = _mapper.Map<Local>(localDto);
+        var localCriado = await _localRepository.Criar(localEntity);
+
+        return _mapper.Map<LocalResponseDto>(localCriado);
+    }
+
+    public async Task<LocalResponseDto> BuscarLocalPorId(Guid id)
+    {
+        var local = await _localRepository.BuscarLocalPorId(id);
+        if (local == null) throw new KeyNotFoundException("Local não encontrado.");
+
+        return _mapper.Map<LocalResponseDto>(local);
+    }
+
+    public async Task<IEnumerable<LocalResponseDto>> BuscarTodos()
+    {
+        var locais = await _localRepository.BuscarTodos();
+        return _mapper.Map<IEnumerable<LocalResponseDto>>(locais);
+    }
+
+    public async Task<LocalResponseDto> Atualizar(LocalRequestDto localDto)
+    {
+        if (localDto == null) throw new ArgumentNullException(nameof(localDto));
+
+        var localEntity = _mapper.Map<Local>(localDto);
+        var localAtualizado = await _localRepository.Atualizar(localEntity);
+        return _mapper.Map<LocalResponseDto>(localAtualizado);
+    }
+
+    public async Task<bool> Deletar(Guid id)
+    {
+        var sucesso = await _localRepository.Deletar(id);
+        if (!sucesso) throw new KeyNotFoundException("Local não encontrado para exclusão.");
+
+        return sucesso;
+    }
+}
