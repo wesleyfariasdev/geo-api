122074e2d4c2b24242b1a50c72d0874e7f63c64e
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-01-03 16:33:47 -0300

    correção

--- GeoApi/Program.cs
+++ GeoApi/Program.cs
@@ -17,7 +17,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<Context>();
-    dbContext.Database.Migrate();  
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
