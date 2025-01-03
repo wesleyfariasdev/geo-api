6ffa44bc0b041af810969077cdc151e9acab72b1
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-01-03 11:53:49 -0300

    adiciona configuração de entidade

---
+++ src/Geo.Infra.Data/EntityConfig/LocalConfigEntity.cs
@@ -0,0 +1,22 @@
+﻿using Geo.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Geo.Infra.Data.EntityConfig;
+
+internal class LocalConfigEntity : IEntityTypeConfiguration<Local>
+{
+    public void Configure(EntityTypeBuilder<Local> builder)
+    {
+        builder.ToTable("LOCAL");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Nome).IsRequired().HasMaxLength(75);
+        builder.Property(x => x.Categoria).IsRequired().HasMaxLength(50);
+
+        builder.Property(x => x.Coordenadas)
+           .IsRequired()
+           .HasColumnType("geography (point, 4326)");
+    }
+}
