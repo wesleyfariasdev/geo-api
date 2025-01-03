ec99d31e98ba9d65849a5f9225f02d8417abcb04
Wesley Farias <wesleyfarias.dev@gmail.com>
2025-01-03 11:36:59 -0300

    criação do model

---
+++ src/Geo.Domain/Models/Local.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace Geo.Domain.Models;
+
+internal class Local
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; }
+    public string Categoria { get; set; }
+    public Point Coordenadas { get; set; }
+}
